49779c312375d68429c4d3529214fd435c368336
mpb00 <72547740+mpb00@users.noreply.github.com>
2022-08-28 15:24:13 +0530

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,21 @@
+import requests as re
+import json
+class read_data(object):
+  def __init__(self, jdata):
+    self.__dict__ = json.loads(jdata)
+    
+city = input("Enter city name:")
+key = '' #api key
+date = input("Enter 1st date format yyyy-mm-dd:")
+date2 = input("Enter 2nd date format yyyy-mm-dd:")
+url = ("http://api.weatherapi.com/v1/history.json?key={}&q={}&dt={}").format(key,city,date)
+url2 = ("http://api.weatherapi.com/v1/history.json?key=e0e95876ccde413788f191824222708&q={}&dt={}").format(city,date2)
+data = re.get(url)
+temp = data.json()['forecast']['forecastday'][0]['day']['avgtemp_c']
+data2 = re.get(url2)
+temp2 = data2.json()['forecast']['forecastday'][0]['day']['avgtemp_c']
+avg = float(temp) + float(temp2)
+avg = avg/2
+print("date 1 temprature is : ",temp)
+print("date 2 temprature is : ",temp2)
+print("average tempature is : {0:.1f}".format(avg))
